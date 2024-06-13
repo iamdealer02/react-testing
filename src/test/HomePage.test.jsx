5dc79e9409468d4d7e411f7fc04926a0b40913b2
Upasana Sharma <sharmaupasana823@gmail.com>
2024-06-13 12:32:12 +0200

    Testing Component after adding deleted note feature

--- src/test/HomePage.test.jsx
+++ src/test/HomePage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import HomePage from "../views/HomePage";
 import * as Services from "../services/utils";
@@ -23,6 +23,11 @@ beforeEach(() => {
         // spying on createNote function before running the test
     // to mock the data that will be returned from the api
     jest.spyOn(Services, "createNote").mockResolvedValue({ note: { title: "Mock Title", content: "This is a mock content" } });
+    // spying the deleteNote function
+    jest.spyOn(Services, "deleteNote").mockResolvedValue({ message: "Note Deleted" });
+    // spying the alert
+    // it is used for create and delete note
+    jest.spyOn(window, "alert").mockImplementation(() => { });
 
 });
 
@@ -48,7 +53,7 @@ test(" create note form render", async () => {
     expect(buttonElement).toBeInTheDocument();
 });
 
-test(" create a new note and add to list", async () => {
+test(" create a new note and add to list ", async () => {
     render(<HomePage />);
     const titleElement = screen.getByPlaceholderText("Title");
     const contentElement = screen.getByPlaceholderText("Content");
@@ -62,18 +67,51 @@ test(" create a new note and add to list", async () => {
 
     userEvent.click(buttonElement);
 
-        expect(await screen.findByText('Mock Title')).toBeInTheDocument();
-        expect(await screen.findByText('This is a mock content')).toBeInTheDocument();
-   
-
+    expect(await screen.findByText('Mock Title')).toBeInTheDocument();
+    expect(await screen.findByText('This is a mock content')).toBeInTheDocument();
 
 });
 
 test("alert when note title and content is empty", async () => {
-    // spying the alert
-    jest.spyOn(window, "alert").mockImplementation(() => { });
+
     render(<HomePage />);
     const buttonElement = screen.getByTestId("create-btn");
     userEvent.click(buttonElement);
     expect(window.alert).toHaveBeenCalledWith("Please fill in all fields");
+});
+
+test("add a note and then delete it", async () => {
+    render(<HomePage />);
+    const titleElement = screen.getByPlaceholderText("Title");
+    const contentElement = screen.getByPlaceholderText("Content");
+    const buttonElement = screen.getByTestId("create-btn");
+
+    userEvent.type(titleElement, 'Mock Title');
+    userEvent.type(contentElement, 'This is a mock content');
+
+    expect(titleElement).toHaveValue('Mock Title');
+    expect(contentElement).toHaveValue('This is a mock content');
+
+    userEvent.click(buttonElement);
+
+    expect(await screen.findByText('Mock Title')).toBeInTheDocument();
+    expect(await screen.findByText('This is a mock content')).toBeInTheDocument();
+    // expect a delete button to be in the document
+    expect(screen.getByTestId('delete-btn')).toBeInTheDocument();
+
+    // Now delete the note
+    const deleteButton = screen.getByTestId('delete-btn');
+    userEvent.click(deleteButton);
+    // Wait for the deletion to complete
+    await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("Note Deleted");
+    });
+
+    await waitFor(() => {
+        expect(screen.queryByText('Mock Title')).not.toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+        expect(screen.queryByText('This is a mock content')).not.toBeInTheDocument();
+    });
 });
