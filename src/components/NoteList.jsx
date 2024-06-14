f3f6ca8107536db55a4d6d0156e97c718b17d292
Upasana Sharma <sharmaupasana823@gmail.com>
2024-06-14 19:02:25 +0200

    Update Note Feature - update note form component - backend call

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './Note';
+import UpdateNote from './UpdateNote';
 
+export default function NoteList({ notes, handleDelete, handleUpdate }) {
+  const [isEditId, setIsEditId] = useState(null);
 
-export default function NoteList({ notes, handleDelete}) {
-  
+  const handleEditClick = (id) => {
+    setIsEditId(id);
+  };
 
   return (
     <div className='note-container'>
@@ -11,10 +15,21 @@ export default function NoteList({ notes, handleDelete}) {
       <span className='noteList-heading'>Your Previous Notes Are Here :)</span>
       {notes.map(note => (
         <div key={note._id} className='notes'>
-            <Note note={note} />
-            <button data-testid='delete-btn' className='button-img' onClick={()=>handleDelete(note._id)}>
-              <img src='https://cdn-icons-png.flaticon.com/128/3221/3221897.png' alt='delete-btn'/>
-            </button>
+          {isEditId === note._id ? (
+            <UpdateNote note={note} setIsEditId={setIsEditId} handleUpdate={handleUpdate} />
+          ) : (
+            <>  
+              <Note note={note} />
+              <div className='note-buttons'>
+                <button data-testid='delete-btn' className='button-img' onClick={() => handleDelete(note._id)}>
+                  <img src='https://cdn-icons-png.flaticon.com/128/3221/3221897.png' alt='delete-btn' />
+                </button>
+                <button data-testid='edit-btn' className='button-img' onClick={() => handleEditClick(note._id)}>
+                  <img src='https://cdn-icons-png.flaticon.com/128/12439/12439280.png' alt='edit-btn' />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       ))}
     </div>
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import NoteList from '../components/NoteList';
 import { fetchAllNotes } from '../services/utils';
 import CreateNote from '../components/CreateNote';
-import { createNote,deleteNote } from '../services/utils';
+import { createNote,deleteNote, updateNote } from '../services/utils';
 
 export default function HomePage() {
     const [notes, setNotes] = useState([]);
@@ -31,22 +31,39 @@ export default function HomePage() {
     };
     // handleDeletenote
     const handleDelete = (_id) => {
-      alert('Note Deleted');
+  
             // database delete req
 
       const data = deleteNote(_id);
       data.then(res => {
         const newNotes = notes.filter(note => note._id !== _id);
         setNotes(newNotes);
+        alert('Note Deleted');
       });
 
 
     };
-  
+    // handleUpdate
+    const handleUpdate = (newNote) => {
+      // database update req
+      const data = updateNote(newNote);
+      data.then(res => {
+        // find the same note in the notes array
+        // tried using map function but for some reason it doesnt get covered by jest
+        const updated_note = notes.find(note => note._id === newNote._id);
+        // update the note
+        updated_note.title = newNote.title;
+        updated_note.content = newNote.content;
+        setNotes([...notes]);
+     
+        alert('Note Updated');
+      });
+
+    };
   return (
     <div>
         <CreateNote handleCreateNote={handleCreateNote} />
-        <NoteList notes={notes} handleDelete={handleDelete} />
+        <NoteList notes={notes} handleDelete={handleDelete} handleUpdate={handleUpdate} />
 
     </div>
   )
